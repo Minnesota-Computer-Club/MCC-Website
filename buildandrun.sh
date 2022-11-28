a1f4a01e8e03743d8f7858778624877a932db1d9
Kenny Harrer <kenny.harrer@icloud.com>
2022-11-28 16:44:40 -0600

    maybe fix

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,4 +1,4 @@
-npm install
-npm run build
+/root/.nvm/versions/node/v16.16.0/bin/npm install
+/root/.nvm/versions/node/v16.16.0/bin/npm run build
 pkill node
-node index.js
+/root/.nvm/versions/node/v16.16.0/bin/node index.js
