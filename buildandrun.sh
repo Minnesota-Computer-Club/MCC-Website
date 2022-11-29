395b0f505f9b90f8c56ec2c9b0282a07b9706451
Kenny Harrer <kenny.harrer@icloud.com>
2022-11-28 18:22:19 -0600

    update node version

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,6 +1,6 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm use 12.13.0
+nvm use 16.16.0
 npm install
 npm run build
 pkill node
