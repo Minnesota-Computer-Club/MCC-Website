a139c22f609cc9ee64dba4f59cf6a1c1eac92b91
Kenny Harrer <kenny.harrer@icloud.com>
2022-12-02 19:35:11 -0600

    fix pull command in cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -1,3 +1,3 @@
-/usr/bin/git pull https://github.com/Minnesota-Computer-Club/MCC-Website.git /home/kennyharrer/nextjs/
+/usr/bin/git pull /home/kennyharrer/nextjs/
 cd /home/kennyharrer/nextjs/
 /usr/bin/sh ./buildandrun.sh
