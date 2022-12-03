4ce1fc78858cdb8a56f9fbef0a335029539cad36
Michael Weiner <vikings412@gmail.com>
2022-12-03 17:13:17 -0600

    fix last remaining bad path

--- components/leaderboard/index.js
+++ components/leaderboard/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import styles from './leaderboard.module.scss';
-import IndividualLeaderboard from '../leaderboard/individuals';
-import TeamLeaderboard from '../leaderboard/teams';
-import SchoolLeaderboard from '../leaderboard/schools';
+import IndividualLeaderboard from '../Leaderboard/individuals';
+import TeamLeaderboard from '../Leaderboard/teams';
+import SchoolLeaderboard from '../Leaderboard/schools';
 import { Nav } from '../Nav/nav';
 
 const MAX_STARS = 25;
