1b1c77e8a68013b93094ae1fd03657c38776789f
root <jobartucz@rochesterschools.org>
2022-12-05 10:52:33 -0600

    fixed if statement

--- buildandrun.sh
+++ buildandrun.sh
@@ -2,7 +2,7 @@
 /root/.nvm/versions/node/v17.0.0/bin/npm run build
 /usr/bin/pkill node
 /root/.nvm/versions/node/v17.0.0/bin/node index.js > /home/kennyharrer/nodelog
-if [ $? == 1 ]
+if [ $? != 0 ]
 then
 	/usr/sbin/sendmail john@bartucz.com < /home/kennyharrer/nodeindexerror.txt
 fi
