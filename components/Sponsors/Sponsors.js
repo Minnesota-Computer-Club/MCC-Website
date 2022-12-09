319f2ecc1f6ed4fdd9c64bfae477702b3d57d6bd
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2022-12-09 10:06:05 -0600

    add better contrast to images

--- components/Sponsors/Sponsors.js
+++ components/Sponsors/Sponsors.js
@@ -9,8 +9,8 @@ export function Sponsors() {
             <img alt="newts" src="/newts.png" />
             <img alt="pi" src="/pi.png" />
             <img alt="rctc" src="/rctc.png" />
-            <img alt="sarg" src="/sarg.png" />
-            <img alt="winona" src="/winona.png" />
+            <img alt="sarg" className={styles.blacklogo} src="/sarg.png" />
+            <img alt="winona" className={styles.blacklogo} src="/winona.png" />
             <img alt="ibm" className={styles.blacklogo} src="/ibm.svg" />
             <img alt="mc" className={styles.blacklogo} src="/mc.svg" />
         </div>
