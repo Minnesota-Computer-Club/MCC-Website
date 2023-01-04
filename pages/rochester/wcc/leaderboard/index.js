a6b470dfe6f6051de1ec8ae75fd9a27dc646c83d
KennyH <71682824+KennyHarrer@users.noreply.github.com>
2023-01-04 11:11:38 -0600

    fix formatting

--- pages/rochester/wcc/leaderboard/index.js
+++ pages/rochester/wcc/leaderboard/index.js
@@ -2,16 +2,16 @@ import Leaderboard from '../../../../components/Leaderboard';
 import { readFile } from 'fs/promises';
 
 export default function RochesterLeaderboard({ AOC, form }) {
-    return <Leaderboard {...{ AOC, form, location: 'Rochester' }} />;
+  return <Leaderboard {...{ AOC, form, location: 'Rochester' }} />;
 }
 
 async function fetchLeaderboard(id) {
-    const req = await fetch(`https://adventofcode.com/2022/leaderboard/private/view/${id}.json`, {
-        headers: {
-            cookie: 'session=53616c7465645f5f6ada3bbcc057af75e3f3cb61ca81f9fc405651e34e3e130cff477b7a6aab1ec9d7ae198fdd8fe30d725a3a2c55bb3370c8b9ef9e20898b0d',
-        },
-    });
-    return await req.json();
+  const req = await fetch(`https://adventofcode.com/2022/leaderboard/private/view/${id}.json`, {
+    headers: {
+      cookie: 'session=53616c7465645f5f6ada3bbcc057af75e3f3cb61ca81f9fc405651e34e3e130cff477b7a6aab1ec9d7ae198fdd8fe30d725a3a2c55bb3370c8b9ef9e20898b0d',
+    },
+  });
+  return await req.json();
 }
 
 const leaderboards = [641987];
@@ -19,41 +19,41 @@ const cached = { AOC: {}, form: {} };
 let last;
 
 export async function getServerSideProps() {
-    if (last > Date.now()) {
-        return { props: { AOC: cached.AOC, form: cached.form } };
-    }
-
-    let AOC = {};
-
-    for (const leaderboardID of leaderboards) {
-        const leaderboardData = await fetchLeaderboard(leaderboardID);
-        AOC = {
-            ...leaderboardData.members,
-            ...AOC,
-        };
-    }
-
-    try {
-        // try to grab production file
-        let formData = await readFile('./python/users_roch.json', { encoding: 'utf-8' });
-        formData = JSON.parse(formData);
-        cached.form = formData;
-    } catch (e) {
-        // fall back to dev file
-        console.log('falling back on dev file... rochester leaderboard');
-        try {
-            let formData = await readFile('./devUsers.json', { encoding: 'utf-8' });
-            formData = JSON.parse(formData);
-            cached.form = formData;
-        } catch (e) {
-            console.log("can't find devusers.json file");
-        }
-    }
-
-    cached.AOC = AOC;
-
-    last = Date.now() + 15 * 60 * 1000;
-    // 15 mins > 60 seconds in a min > 1000 ms in a second
-
+  if (last > Date.now()) {
     return { props: { AOC: cached.AOC, form: cached.form } };
+  }
+
+  let AOC = {};
+
+  for (const leaderboardID of leaderboards) {
+    const leaderboardData = await fetchLeaderboard(leaderboardID);
+    AOC = {
+      ...leaderboardData.members,
+      ...AOC,
+    };
+  }
+
+  try {
+    // try to grab production file
+    let formData = await readFile('./python/users_roch.json', { encoding: 'utf-8' });
+    formData = JSON.parse(formData);
+    cached.form = formData;
+  } catch (e) {
+    // fall back to dev file
+    console.log('falling back on dev file... rochester leaderboard');
+    try {
+      let formData = await readFile('./devUsers.json', { encoding: 'utf-8' });
+      formData = JSON.parse(formData);
+      cached.form = formData;
+    } catch (e) {
+      console.log('can\'t find devusers.json file');
+    }
+  }
+
+  cached.AOC = AOC;
+
+  last = Date.now() + 15 * 60 * 1000;
+  // 15 mins > 60 seconds in a min > 1000 ms in a second
+
+  return { props: { AOC: cached.AOC, form: cached.form } };
 }
